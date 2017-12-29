2982f88eec968bc109af297eab5920f36ce7cb2d
Alexandr Kravchenko <sasha121196@mail.ru>
2017-12-29 18:21:06 +0300

    Update TypeFunctions.hs

--- src/Construction/Internal/TypeFunctions.hs
+++ src/Construction/Internal/TypeFunctions.hs
@@ -3,12 +3,12 @@
 
 module Construction.Internal.TypeFunctions where
 
-import qualified Data.Map                        as M ((!))
+import qualified Data.Map                        as M ((!), member)
 import           Data.Text                       (pack)
 import           Data.Map                        (member, fromList)
-import           Data.Set                        (Set (..), elemAt, delete, singleton, toList)
+import           Data.Set                        (Set (..), elemAt, delete, singleton, toList, union, insert, map)
 import           Construction.Internal.Types
-import           Construction.Internal.Functions hiding (Context, substitute)
+import           Construction.Internal.Functions hiding (Context)
 
 -- Split a set of elements to the first element and rest set
 split :: Ord a => Set a -> (a, Set a)
@@ -22,51 +22,19 @@ ctx ! x | member x (getCtx ctx) = Just $ getCtx ctx M.! x
 
 -- Something we can perform substitution with
 class Substitutable a where
-  substitute :: Substitution -> a -> a
+  substitute1 :: Substitution -> a -> a
 
 -- Substitution in context
 --   [a:=t]empty       => empty
 --   [a:=t]{x:t1 ... } => {x:([a:=t]t1) ... }
 instance Substitutable Context where
-  substitute = undefined
+  substitute1 s ctx = Context (fmap (substitute1 s) (getCtx ctx))
 
 -- Substitution in type:
 --   [a:=t] a     => t
 --   [a:=t] b     => b
 --   [a:=t](r->p) => ([a:=t]r)->([a:=t]p)
 instance Substitutable Type where
-  substitute = undefined  
+  substitute1 s t@(TVar name) = if M.member name (getSubs s) then (getSubs s) M.! name else t
 
--- Compose two substitutions
--- S@[a1 := t1, ...] . [b1 := s1 ...] = [b1 := S(s1) ... a1 := t1 ...]
-compose :: Substitution -> Substitution -> Substitution
-compose bc ab = undefined
-
--- Create new context from free variables of some term
-contextFromTerm :: Term -> Context
-contextFromTerm term = Context $ fromList $ zip (toList $ free term) vars
-  where
-    vars = fmap (TVar . pack . ('a':) . show) [1..] 
-
--- Find a substitution that can solve the set of equations
-u :: Set Equation -> Maybe Substitution
-u set | null set  = pure mempty
-      | otherwise = let (x, rest) = split set
-                    in undefined
-
--- Generate equations set from some term
--- NB: you can use @fresh@ function to generate type names
-e :: Context -> Term -> Type -> Maybe (Set Equation)
-e ctx term tpe = case term of
-                   Var{..} -> (\x -> singleton (tpe,x)) <$> ctx ! var
-                   App{..} -> undefined
-                   Lam{..} -> undefined
-
--- Find a principal pair of some term if exists
-pp :: Term -> Maybe (Context, Type)
-pp term = do let ctx = contextFromTerm term
-             let tpe = TVar "r"
-             eqs <- e ctx term tpe
-             subs <- u eqs
-             pure (substitute subs ctx, substitute subs tpe)
-© 2017 GitHub, Inc.
+  substitute1 s (TArr n m) = TArr (substitute1 s n) (substitute1 s m)
